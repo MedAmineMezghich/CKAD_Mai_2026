f7fec640bec53eebf65a80092afbf27e9016b0c8
Mohamed Amine MEZGHICH <git@mezghich.com>
2026-05-30 17:51:13 +0100

    exemple avec app spring

---
+++ Day1_Docker/labs/lab_3_site_sip_academy_spring/sip_academy/src/test/java/com/sip/app/SipAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sip.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
